ab69f5b8ec468b5f54e914cdb4546a6fc536a517
Feng Ji Min (Ferris) <feng_jimin@foxmail.com>
2025-10-12 22:07:10 +0800

    feat: add swagger doc to auth and login api

--- src/main/java/com/ocbc/ms/controller/UserController.java
+++ src/main/java/com/ocbc/ms/controller/UserController.java
@@ -1,23 +1,34 @@
 package com.ocbc.ms.controller;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * only for login, no need to implement it for our demo
+ * Controller for user related operations
  */
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
+@Tag(name = "User Management", description = "APIs for user authentication")
 public class UserController {
 
     /**
-     * login, will do it later
+     * User login endpoint
+     * 
+     * @return Success message if login is successful
      */
     @PostMapping("/login")
+    @Operation(summary = "User login", description = "Authenticate user and return success message")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Successfully logged in"),
+        @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     public ResponseEntity<String> login() {
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
     }
